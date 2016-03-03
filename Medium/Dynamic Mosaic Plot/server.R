8b262b2d5b3d87d5f51a50f47963fa13e86129cf
cryptomanic <deepakyadav.iitbhu@gmail.com>
2016-03-03 10:32:44 +0530

    select random observations

--- Medium/Dynamic Mosaic Plot/server.R
+++ Medium/Dynamic Mosaic Plot/server.R
@@ -8,20 +8,21 @@ shinyServer(function(input, output) {
     # number of observations selected by user
     nob <- input$count
     
+    # randomly select observations
+    obs_selected <- sample(1:nrow(happy), nob)
+    
     # which divider is selected by user
     # default divider is mosaic
     if(input$divider == "stacked") {
       
-      prodplot(happy[1:nob, ], ~ happy + sex, stacked())
+      prodplot(happy[obs_selected, ], ~ happy + sex, stacked())
       
     } else {
       
-      prodplot(happy[1:nob, ], ~ happy + sex)
+      prodplot(happy[obs_selected, ], ~ happy + sex)
       
     } 
     
   })
-  
-  
   
 })
